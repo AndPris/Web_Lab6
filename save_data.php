d759a5668d5a0c654672affaa70e5f8e7991c418
Andrii <andpris527@gmail.com>
2023-11-27 20:22:34 +0200

    Add asynchronous data saving

--- save_data.php
+++ save_data.php
@@ -1,15 +1,15 @@
 <?php 
 	$conn = new mysqli("sql106.infinityfree.com", "if0_35501733", "zbE95UqqYTVM", "if0_35501733_lab6_tabs");
 	if ($conn->connect_error)
-    die("Connection failed: " . $conn->connect_error);
+    	die("Connection failed: " . $conn->connect_error);
     				
-    $title = $_POST['title'];
-    $data = $_POST['data'];
+    $title = mysqli_real_escape_string($conn, $_POST['title']);
+    $data = mysqli_real_escape_string($conn, $_POST['data']);
 
 	$sql="INSERT INTO tabs(title, data) VALUES('$title', '$data');";
 	if($conn->query($sql))
-		echo "Value inserted";
+		echo "Data saved";
 	else
-		echo "Error insertion";
+		echo "Saving error";
 	$conn->close();
 ?>
