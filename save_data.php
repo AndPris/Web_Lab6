1cb9827efd283e6318133923a9a4b6af1d41d2e2
Andrii <andpris527@gmail.com>
2024-01-05 13:54:04 +0200

    Hide sensetive information

--- save_data.php
+++ save_data.php
@@ -1,5 +1,8 @@
-<?php 
-	$conn = new mysqli("sql106.infinityfree.com", "if0_35501733", "zbE95UqqYTVM", "if0_35501733_lab6_tabs");
+<?php
+	require_once 'config.php';
+
+	$conn = new mysqli(HOST_NAME, USER_NAME, PASSWORD, DATABASE);
+
 	if ($conn->connect_error)
     	die("Connection failed: " . $conn->connect_error);
     				
